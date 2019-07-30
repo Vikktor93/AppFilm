5f3ff0ec1a2724b1f04277c358e1437947678977
Vikktor93 <viktorsaldivia93@gmail.com>
2019-07-29 21:47:20 -0400

    git

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: "MyIMDB",
+      title: "My IMDB",
       home: _mutaEstados()
       );
   }
